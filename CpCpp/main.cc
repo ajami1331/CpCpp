0214c98cf8f9f57951ba550db6feb25d58d3b30b
CLown1331 <clown1331@gmail.com>
2021-04-24 04:23:02 +0600

    Hello world

---
+++ CpCpp/main.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include "utils.h"
+#include "solution.h"
+
+int main(int argc, char* argv[])
+{
+#ifdef CLown1331
+    const clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+    solution::Solve();
+#ifdef CLown1331
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", static_cast<double>(clock() - tStart) / CLOCKS_PER_SEC);
+    utils::CreateFileForSubmission();
+#endif
+    return 0;
+}
