ee65723ed512663638bc15622bc153cce75679cb
Araf Al Jami <clown1331@gmail.com>
2023-02-17 21:46:37 +0000

    Add aho corasick

--- library/AhoCorasick.h
+++ library/AhoCorasick.h
@@ -1,31 +1,36 @@
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
-#include <queue>
-#include <vector>
 #ifndef AhoCorasick_h
 #define AhoCorasick_h 1
+
+#include <queue>
+#include <vector>
+#include <algorithm>
+
 namespace library
 {
+
 template <int MAX> struct AhoCorasick
 {
     std::vector<int> mark[MAX + 7];
     int state, failure[MAX + 7];
     int trie[MAX + 7][26];
+
     AhoCorasick()
     {
         Init();
     }
+
     void Init()
     {
         mark[0].clear();
         std::fill(trie[0], trie[0] + 26, -1);
         state = 0;
     }
+
     int Value(char c)
     {
         return c - 'a';
     }
+
     void Add(char *s, int t)
     {
         int root = 0, id;
@@ -42,6 +47,7 @@ template <int MAX> struct AhoCorasick
         }
         mark[root].push_back(t);
     }
+
     void ComputeFailure()
     {
         std::queue<int> Q;
@@ -75,59 +81,7 @@ template <int MAX> struct AhoCorasick
         }
     }
 };
+
 } // namespace library
+
 #endif
-#ifndef solution_h
-#define solution_h 1
-namespace solution
-{
-using namespace std;
-const int sz = 1e6 + 10;
-const int MAX = 505 * 505 + 100;
-char inp[sz], s[505][505];
-int cnt[505];
-library::AhoCorasick<MAX> automata;
-void countFreq()
-{
-    for (int i = 0, root = 0, id; inp[i]; i++)
-    {
-        id = automata.Value(inp[i]);
-        root = automata.trie[root][id];
-        if (root == 0)
-            continue;
-        for (int i = 0; i < automata.mark[root].size(); i++)
-            cnt[automata.mark[root][i]]++;
-    }
-}
-void Solve()
-{
-    int t, n, cs;
-    scanf("%d", &t);
-    for (int cs = 1; cs <= t; cs++)
-    {
-        scanf("%d", &n);
-        scanf("%s", inp);
-        automata.Init();
-        for (int i = 0; i < n; i++)
-        {
-            scanf("%s", s[i]);
-            automata.Add(s[i], i);
-        }
-        automata.ComputeFailure();
-        memset(cnt, 0, sizeof cnt);
-        countFreq();
-        printf("Case %d:\n", cs);
-        for (int i = 0; i < n; i++)
-        {
-            printf("%d\n", cnt[i]);
-        }
-    }
-}
-} // namespace solution
-#endif
-#define _CRT_SECURE_NO_WARNINGS
-int main(int argc, char *argv[])
-{
-    solution::Solve();
-    return 0;
-}
--- solution.h
+++ solution.h
@@ -2,34 +2,55 @@
 #define solution_h 1
 
 #include <cstdio>
-#include "library/LCA.h"
+#include <cstring>
+#include "library/AhoCorasick.h"
 
 namespace solution
 {
 using namespace std;
 
-const int sz = 2e5 + 10;
-const int INF = 1e9 + 10;
+const int sz = 1e6 + 10;
+const int MAX = 505 * 505 + 100;
+char inp[sz], s[505][505];
+int cnt[505];
+
+library::AhoCorasick<MAX> automata;
+
+void countFreq()
+{
+    for (int i = 0, root = 0, id; inp[i]; i++)
+    {
+        id = automata.Value(inp[i]);
+        root = automata.trie[root][id];
+        if (root == 0)
+            continue;
+        for (int i = 0; i < automata.mark[root].size(); i++)
+            cnt[automata.mark[root][i]]++;
+    }
+}
 
 void Solve()
 {
-    int n, q;
-    scanf("%d %d", &n, &q);
-    library::LCA lca(n);
-    for (int i = 1; i < n; i++)
+    int t, n, cs;
+    scanf("%d", &t);
+    for (int cs = 1; cs <= t; cs++)
     {
-        int v;
-        scanf("%d", &v);
-        lca.AddEdge(i, v);
-    }
-
-    lca.Build(0);
-
-    while (q--)
-    {
-        int u, v;
-        scanf("%d %d", &u, &v);
-        printf("%d\n", lca.Query(u, v));
+        scanf("%d", &n);
+        scanf("%s", inp);
+        automata.Init();
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%s", s[i]);
+            automata.Add(s[i], i);
+        }
+        automata.ComputeFailure();
+        memset(cnt, 0, sizeof cnt);
+        countFreq();
+        printf("Case %d:\n", cs);
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d\n", cnt[i]);
+        }
     }
 }
 } // namespace solution
