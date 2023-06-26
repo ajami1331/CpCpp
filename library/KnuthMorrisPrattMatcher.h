1be64cccb484a495c45f6c3c2927a1a7eb5bc62c
Araf Al Jami <clown1331@gmail.com>
2023-06-26 12:57:07 +0100

    Add KnuthMorrisPrattMatcher.h

--- library/KnuthMorrisPrattMatcher.h
+++ library/KnuthMorrisPrattMatcher.h
@@ -1,19 +1,15 @@
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <utility>
-#include <vector>
 #ifndef KnuthMorrisPrattMatcher_h
 #define KnuthMorrisPrattMatcher_h 1
+
+#include <algorithm>
+#include <cstring>
+#include <vector>
+
 template <size_t MAXLEN> struct KnuthMorrisPrattMatcher
 {
     char m[MAXLEN];
     int table[MAXLEN], lenM;
+
     void Init(const char *str, int n)
     {
         strncpy(m, str, n);
@@ -42,6 +38,7 @@ template <size_t MAXLEN> struct KnuthMorrisPrattMatcher
             }
         }
     }
+
     std::vector<int> MatchString(const char *s, int lenS)
     {
         int i = 0, j = 0;
@@ -73,42 +70,5 @@ template <size_t MAXLEN> struct KnuthMorrisPrattMatcher
         return matches;
     }
 };
+
 #endif
-#ifndef solution_h
-#define solution_h 1
-namespace solution
-{
-using namespace std;
-using ll = long long;
-using ull = unsigned long long;
-const int sz = 1e6 + 10;
-int n;
-char s[sz];
-void Solve()
-{
-    int cs = 0;
-    while (cin >> n && n > 0)
-    {
-        cin >> s;
-        KnuthMorrisPrattMatcher<sz> matcher;
-        matcher.Init(s, n);
-        cout << "Test case #" << (++cs) << "\n";
-        for (int i = 0; i < n; i++)
-        {
-            int len = i + 1 - matcher.table[i];
-            if ((i + 1) % len == 0 && (i + 1) / len > 1)
-            {
-                cout << (i + 1) << " " << (i + 1) / len << "\n";
-            }
-        }
-        cout << "\n";
-    }
-}
-} // namespace solution
-#endif
-#define _CRT_SECURE_NO_WARNINGS
-int main(int argc, char *argv[])
-{
-    solution::Solve();
-    return 0;
-}
--- solution.h
+++ solution.h
@@ -8,9 +8,10 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <unordered_map>
 #include <vector>
-#include "library/PolyHash.h"
-#include "library/SmallestStringRotation.h"
+#include <utility>
+#include "library/KnuthMorrisPrattMatcher.h"
 
 namespace solution
 {
@@ -18,36 +19,28 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 
-const int sz = 1e4 + 10;
+const int sz = 1e6 + 10;
 int n;
-string s;
-library::SmallestStringRotation<sz> ssr;
-set <ull> st;
+char s[sz];
 
 void Solve()
 {
-    scanf("%d", &n);
-    while (n--)
+    int cs = 0;
+    while (cin >> n && n > 0)
     {
         cin >> s;
-        ll ans = 0;
-        st.clear();
-        for (int i = 0; i < s.size(); i++)
+        KnuthMorrisPrattMatcher<sz> matcher;
+        matcher.Init(s, n);
+        cout << "Test case #" << (++cs) << "\n";
+        for (int i = 0; i < n; i++)
         {
-            string h;
-            for (int j = i; j < s.size(); j++)
+            int len = i + 1 - matcher.table[i];
+            if ((i + 1) % len == 0 && (i + 1) / len > 1)
             {
-                h += s[j];
-                int rotation = ssr(h);
-                string p = h;
-                rotate(p.begin(), p.begin() + rotation, p.end());
-                library::PolyHash<sz> ph(vector<char>(p.begin(), p.end()));
-                ull hash = ph.GetHash(0, p.size() - 1);
-                st.insert(hash);
+                cout << (i + 1) << " " << (i + 1) / len << "\n";
             }
         }
-
-        printf("%d\n", (int)st.size());
+        cout << "\n";
     }
 }
 
