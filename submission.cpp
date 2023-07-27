13c4d940fec14ffbc7f28c1da8a1b3173ba5d050
Araf Al Jami <clown1331@gmail.com>
2023-07-28 00:51:27 +0100

    Add tools to generate html list

--- submission.cpp
+++ submission.cpp
@@ -18,65 +18,138 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 const int sz = 2e5 + 105;
-int t, n, m;
-vector<pair<int, int>> g[sz];
-bool vis[sz];
-int color[sz];
-bool dfs(int u, int c)
+int t, n, m, k, h;
+ll ar[sz];
+bool mark[sz];
+bool check_first()
 {
-    vis[u] = true;
-    color[u] = c;
-    for (auto &v : g[u])
+    fill(mark, mark + n + 2, false);
+    vector <ll> v;
+    for (int i = 1; i + 1  < n; i++)
     {
-        if (!vis[v.first])
+        if (ar[i] - ar[i - 1] <= n)
         {
-            dfs(v.first, c + v.second);
-        }
-        else if (color[v.first] != c + v.second)
-        {
-            return false;
+            v.emplace_back(ar[i] - ar[i - 1]);
+            mark[v.back()] = true;
         }
     }
-    return true;
+    vector <ll> pai_nai_khuje;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!mark[i])
+        {
+            pai_nai_khuje.emplace_back(i);
+        }
+    }
+    bool fl = true;
+    if (pai_nai_khuje.size() != 2 || pai_nai_khuje[0] + pai_nai_khuje[1] != ar[0])
+    {
+        fl = false;
+    }
+    v.emplace_back(pai_nai_khuje[0]);
+    v.emplace_back(pai_nai_khuje[1]);
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n && fl; i++)
+    {
+        fl = fl && (v[i] == i + 1);
+    }
+    return fl;
+}
+bool check_mittel()
+{
+    fill(mark, mark + n + 2, false);
+    vector <ll> v;
+    if (ar[0] <= n)
+    {
+        v.emplace_back(ar[0]);
+        mark[ar[0]] = true;
+    }
+    for (int i = 1; i + 1  < n; i++)
+    {
+        if (ar[i] - ar[i - 1] <= n && !mark[ar[i] - ar[i - 1]])
+        {
+            v.emplace_back(ar[i] - ar[i - 1]);
+            mark[v.back()] = true;
+        }
+    }
+    vector <ll> pai_nai_khuje;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!mark[i])
+        {
+            pai_nai_khuje.emplace_back(i);
+        }
+    }
+    bool fl = true;
+    if (pai_nai_khuje.size() != 2)
+    {
+        fl = false;
+    }
+    v.emplace_back(pai_nai_khuje[0]);
+    v.emplace_back(pai_nai_khuje[1]);
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n && fl; i++)
+    {
+        fl = fl && (v[i] == i + 1);
+    }
+    return fl;
 }
 void Solve()
 {
     scanf("%d", &t);
-    while (t-- && scanf("%d %d", &n, &m) == 2)
+    while (t-- && scanf("%d", &n) == 1)
     {
-        for (int i = 0; i <= n; i++)
+        for (int i = 0; i + 1 < n; i++)
         {
-            g[i].clear();
-            vis[i] = false;
-            color[i] = 0;
+            scanf("%lld", &ar[i]);
         }
-        for (int i = 0; i < m; i++)
+        ll sum = n;
+        sum = (sum * (sum + 1ll)) / 2ll;
+        int ind = max_element(ar, ar + n - 1) - ar;
+        if (ind != n - 2)
         {
-            int x, y, w;
-            scanf("%d %d %d", &x, &y, &w);
-            g[x].emplace_back(y, w);
-            g[y].emplace_back(x, -w);
+            puts("NO");
+            continue;
         }
-        bool ok = true;
-        for (int i = 1; i <= n && ok; i++)
+        if (ar[ind] > sum)
         {
-            if (!vis[i])
+            puts("NO");
+            continue;
+        }
+        if (ar[ind] < sum)
+        {
+            vector <ll> v;
+            v.emplace_back(ar[0]);
+            for (int i = 1; i + 1  < n; i++)
             {
-                ok = ok && dfs(i, 1);
+                v.emplace_back(ar[i] - ar[i - 1]);
             }
-        }
-        for (int i = 1; i <=n && ok ;i++)
-        {
-            for (auto &v : g[i])
+            v.emplace_back(sum - ar[ind]);
+            sort(v.begin(), v.end());
+            bool fl = true;
+            for (int i = 0; i < n && fl; i++)
             {
-                if (color[v.first] != color[i] + v.second)
-                {
-                    ok = false;
-                    break;
-                }
+                fl = fl && (v[i] == i + 1);
             }
+            puts(fl ? "YES" : "NO");
+            continue;
         }
-        puts(ok ? "YES" : "NO");
+        if (ar[ind] != sum)
+        {
+            puts("NO");
+            continue;
+        }
+        if (check_first())
+        {
+            puts("YES");
+            continue;
+        }
+        if (check_mittel())
+        {
+            puts("YES");
+            continue;
+        }
+        puts("NO");
     }
 }
 } // namespace solution
---
+++ tools/book-generator.cpp
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+namespace fs = std::filesystem;
+
+std::vector<std::string> getFiles(std::string path)
+{
+    std::vector<std::string> files;
+    for (const auto &entry : fs::directory_iterator(path))
+    {
+        if (entry.is_directory())
+        {
+            auto subfiles = getFiles(entry.path().string());
+            files.insert(files.end(), subfiles.begin(), subfiles.end());
+        }
+        else
+        {
+            files.emplace_back(entry.path().string());
+        }
+    }
+    return files;
+}
+
+int main(int argc, char *argv[])
+{
+    auto files = getFiles(CP_LIBRARY_PATH);
+    std::string highlightjsVersion = "11.8.0";
+    std::string header =
+        std::string("") + "<!DOCTYPE html>\n" + "<html>\n" + "    <head>\n" + "    <meta charset=\"utf-8\">\n" +
+        "    <meta name=\"viewport\" content=\"width=device-width\">\n" + "    <title>Algorithms</title>\n" +
+        "    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/" +
+        highlightjsVersion + "/styles/default.min.css\">\n" +
+        "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/" + highlightjsVersion +
+        "/highlight.min.js\"></script>\n" + "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/" +
+        highlightjsVersion + "/languages/go.min.js\"></script>\n" + "    </head>\n" + "<body>\n";
+    std::string footer = "</body></html>";
+    fs::remove_all(TOOLS_BOOK_GENERATOR_OUT_DIR);
+    fs::create_directory(TOOLS_BOOK_GENERATOR_OUT_DIR);
+    std::string output_file_name = std::string("") + TOOLS_BOOK_GENERATOR_OUT_DIR + "index.html";
+    std::ofstream outFile;
+    std::cout << "Writing to " << output_file_name << std::endl;
+    outFile.open(output_file_name);
+    outFile << header;
+    outFile << "<h1>Algorithms</h1>\n";
+    outFile << "<h2 id=\"table-of-content\">Table of Contents</h2>\n";
+    outFile << "<ul>\n";
+    for (auto file : files)
+    {
+        fs::path path(file);
+        std::string filename = path.filename().string();
+        outFile << "<li><a href=\"#" + filename + "\">" + filename + "</a></li>\n";
+    }
+    outFile << "</ul>\n";
+    for (auto file : files)
+    {
+        fs::path path(file);
+        std::string filename = path.filename().string();
+        outFile << "<h2 id=\"" + filename + "\">" + filename + "</h2>\n";
+        outFile << "<a href=\"#table-of-content\">^top</a>\n";
+        outFile << "<pre><code>\n";
+        std::ifstream t(file);
+        std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+        outFile << str;
+        outFile << "</code></pre>\n";
+    }
+    outFile << footer;
+    return 0;
+}
