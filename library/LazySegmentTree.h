77610624860eb1c7c063d4598d5f9bec1e699ef6
Araf Al Jami <clown1331@gmail.com>
2023-10-24 00:07:08 +0100

    Add LazySegmentTree

---
+++ library/LazySegmentTree.h
@@ -0,0 +1,125 @@
+#ifndef LAZY_SEGMENT_TREE_H
+#define LAZY_SEGMENT_TREE_H 1
+
+#include <algorithm>
+#include <functional>
+
+
+namespace library
+{
+#if __cplusplus >= 201703L
+template <class S, auto op, auto e, class F, auto mapping, auto composition, auto id, size_t sz> class LazySegmentTree
+{
+    static_assert(std::is_convertible_v<decltype(op), std::function<S(S, S)>>, "op must work as S(S, S)");
+    static_assert(std::is_convertible_v<decltype(e), std::function<S()>>, "e must work as S()");
+    static_assert(std::is_convertible_v<decltype(mapping), std::function<S(F, S)>>, "mapping must work as F(F, S)");
+    static_assert(std::is_convertible_v<decltype(composition), std::function<F(F, F)>>,
+                  "compostiion must work as F(F, F)");
+    static_assert(std::is_convertible_v<decltype(id), std::function<F()>>, "id must work as F()");
+
+#else
+
+template <class S, S (*op)(S, S), S (*e)(), class F, S (*mapping)(F, S), F (*composition)(F, F), F (*id)(), size_t sz>
+class LazySegmentTree
+{
+
+#endif
+  private:
+    S tree[sz * 4];
+    F lazy[sz * 4];
+
+  public:
+    LazySegmentTree()
+    {
+        Reset();
+    }
+    
+    void Reset()
+    {
+        std::fill(tree, tree + sz * 4, e());
+        std::fill(lazy, lazy + sz * 4, id());
+    }
+
+    void Build(int node, int begin, int end)
+    {
+        tree[node] = e();
+        lazy[node] = id();
+        if (begin == end)
+        {
+            return;
+        }
+        int left = node << 1;
+        int right = left | 1;
+        int mid = (begin + end) >> 1;
+        Build(left, begin, mid);
+        Build(right, mid + 1, end);
+        tree[node] = op(tree[left], tree[right]);
+    }
+
+    void Build(int node, int begin, int end, S *arr)
+    {
+        tree[node] = e();
+        lazy[node] = id();
+        if (begin == end)
+        {
+            tree[node] = arr[begin];
+            return;
+        }
+        int left = node << 1;
+        int right = left | 1;
+        int mid = (begin + end) >> 1;
+        Build(left, begin, mid, arr);
+        Build(right, mid + 1, end, arr);
+        tree[node] = op(tree[left], tree[right]);
+    }
+
+    inline void Push(int node, int begin, int end)
+    {
+        tree[node] = mapping(lazy[node], tree[node]);
+        if (begin != end)
+        {
+            int left = node << 1;
+            int right = left | 1;
+            lazy[left] = composition(lazy[node], lazy[left]);
+            lazy[right] = composition(lazy[node], lazy[right]);
+        }
+        lazy[node] = id();
+    }
+
+    inline void Update(int node, int begin, int end, int l, int r, F x)
+    {
+        Push(node, begin, end);
+        if (r < begin || end < l)
+            return;
+        if (l <= begin && end <= r)
+        {
+            lazy[node] = composition(x, lazy[node]);
+            Push(node, begin, end);
+            return;
+        }
+        int left = node << 1;
+        int right = left | 1;
+        int mid = (begin + end) >> 1;
+        Update(left, begin, mid, l, r, x);
+        Update(right, mid + 1, end, l, r, x);
+        tree[node] = op(tree[left], tree[right]);
+    }
+
+    inline S Query(int node, int begin, int end, int l, int r)
+    {
+        Push(node, begin, end);
+        if (r < begin || end < l)
+            return e();
+        if (l <= begin && end <= r)
+        {
+            return tree[node];
+        }
+        int left = node << 1;
+        int right = left | 1;
+        int mid = (begin + end) >> 1;
+        return op(Query(left, begin, mid, l, r), Query(right, mid + 1, end, l, r));
+    }
+};
+} // namespace library
+
+#endif // LAZY_SEGMENT_TREE_H
--- solution.h
+++ solution.h
@@ -2,6 +2,7 @@
 #define solution_h 1
 
 #include <algorithm>
+#include <climits>
 #include <cmath>
 #include <cstdio>
 #include <cstring>
@@ -13,7 +14,8 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
-#include "library/FenwickTree.h"
+#include "library/LazySegmentTree.h"
+#include "library/atcoder/lazysegtree"
 
 namespace solution
 {
@@ -22,125 +24,101 @@ using ll = long long;
 using ull = unsigned long long;
 
 const int sz = 2e5 + 105;
-int t, n, m;
-char s[sz];
-int type, l, r, x;
+int n, m;
 
-struct mod_plus : public binary_function<int, int, int>
+struct S
 {
-    int operator()(const int &x, const int &y) const
-    {
-        return (x + y) % 26;
-    }
+    ll val;
+    ll cnt;
 };
 
-struct mod_minus : public binary_function<int, int, int>
+struct F
 {
-    int operator()(const int &x, const int &y) const
+    int type;
+    ll x;
+    int step;
+    F()
     {
-        return (x - y + 26) % 26;
+        type = 0;
+        x = 0;
+        step = -1;
     }
+    F(int type, ll x, int step) : type(type), x(x), step(step) {}
 };
 
-library::FenwickTree<int, sz, mod_plus, mod_minus> ft;
-
-set <int> two, three;
-
-void push(int l, int r)
+S op(S a, S b)
 {
-    l = max(l, 1);
-    r = min(r, n);
-    for (int i = l; i <= r; ++i)
-    {
-        if (i + 1 <= n && ft.Query(i) == ft.Query(i + 1))
-        {
-            two.insert(i);
-        }
-        else 
-        {
-            two.erase(i);
-        }
-        if (i + 2 <= n && ft.Query(i) == ft.Query(i + 2))
-        {
-            three.insert(i);
-        }
-        else
-        {
-            three.erase(i);
-        }
-    }
+    return {a.val + b.val, a.cnt + b.cnt};
 }
 
-void print()
+S e()
 {
-    for (int i = 1; i <= n; ++i)
-    {
-        printf("%c", ft.Query(i) + 'a');
-    }
-    printf("\n");
+    return {0, 0};
 }
+
+S mapping(F f, S x)
+{
+    if (f.type == 0)
+        return {x.val + (f.x * x.cnt), x.cnt};
+    return {f.x * x.cnt, x.cnt};
+}
+
+F composition(F f, F g)
+{
+    if (f.step >= g.step)
+    {
+        if (f.type == 0 && g.type == 0)
+            return {0, f.x + g.x, f.step};
+        return f;
+    }
+    if (f.type == 0 && g.type == 0)
+        return {0, f.x + g.x, g.step};
+    return g;
+}
+
+F id()
+{
+    return {1, 0, -1};
+}
+
+library::LazySegmentTree<S, op, e, F, mapping, composition, id, sz> st;
 
 void Solve()
 {
-    scanf("%d", &t);
-    while (t-- && scanf("%d %d", &n, &m) == 2)
+    scanf("%d %d", &n, &m);
+    vector<S> a(n);
+    for (auto &x : a)
     {
-        s[0] = 0;
-        scanf("%s", s + 1);
-        ft.Reset();
-        two.clear();
-        three.clear();
-        for (int i = 1; i <= n; ++i)
+        scanf("%lld", &x.val);
+        x.cnt = 1; 
+    }
+    st.Build(1, 0, n - 1, a.data());
+    atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> seg(a);
+    int step = 0;
+    while (m--)
+    {
+        step++;
+        int type, l, r, x;
+        scanf("%d", &type);
+        if (type == 1)
         {
-            ft.Update(i, s[i] - s[i - 1] + 26);
-            if (i + 1 <= n && s[i] == s[i + 1])
-            {
-                two.insert(i);
-            }
-            if (i + 2 <= n && s[i] == s[i + 2])
-            {
-                three.insert(i);
-            }
+            scanf("%d %d %d", &l, &r, &x);
+            --l, --r;
+            seg.apply(l, r + 1, {1, x, step});
+            st.Update(1, 0, n - 1, l, r, {1, x, step});
         }
-        while (m--)
+        else if (type == 2)
         {
-            // print();
-            scanf("%d", &type);
-            
-            if (type == 1)
-            {
-                scanf("%d %d %d", &l, &r, &x);
-                x %= 26;
-                ft.RangeUpdate(l, r, x);
-                push(l - 5, l + 5);
-                push(r - 5, r + 5);
-            }
-            else
-            {
-                scanf("%d %d", &l, &r);
-                int ans = 0;
-                auto it = two.lower_bound(l);
-                if (it != two.end() && *it + 1 <= r)
-                {
-                    ans = 1;
-                }
-                else
-                {
-                    it = three.lower_bound(l);
-                    if (it != three.end() && *it + 2 <= r)
-                    {
-                        ans = 1;
-                    }
-                }
-                if (ans > 0)
-                {
-                    printf("NO\n");
-                }
-                else
-                {
-                    printf("YES\n");
-                }
-            }
+            scanf("%d %d %d", &l, &r, &x);
+            --l, --r;
+            seg.apply(l, r + 1, {2, x, step});
+            st.Update(1, 0, n - 1, l, r, {2, x, step});
+        }
+        else
+        {
+            scanf("%d %d", &l, &r);
+            --l, --r;
+            printf("%lld\n", seg.prod(l, r + 1).val);
         }
     }
 }
